0dc89ebf22e7b0918c34fccc01e7fac35a98683d
Keshav Prajapati <keshavpj1711@gmail.com>
2024-07-15 20:59:50 +0530

    showing positions to users

--- main.py
+++ main.py
@@ -3,6 +3,18 @@ rows, cols = (3, 3)
 board = [[0 for i in range(cols)] for j in range(rows)]
 
 
+def show_position():
+  for i in range(0, 3):
+    for j in range(0, 3):
+      board[i][j] = (i*3)+(j+1)
+
+  board_layout()
+
+  # after showing we need to wipe all the position to " "
+  for i in range(0, 3):
+    for j in range(0, 3):
+      board[i][j] = " "  
+
 def board_layout():
   print(f" {board[0][0]} | {board[0][1]} | {board[0][2]}")
   print("-----------")
@@ -10,10 +22,17 @@ def board_layout():
   print("-----------")
   print(f" {board[2][0]} | {board[2][1]} | {board[2][2]}")
 
-game_logic()
+
+def game_logic():
+  pass
+
 
 def main():
-  pass
+  print("Welcome!!")
+  show_position()
+
+  # Our next goal is to place elements
+  
 
 if __name__ == "__main__":
   main()
