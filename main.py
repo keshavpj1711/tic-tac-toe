546825ec19a917ae310420e3f05aa27a92352c04
Keshav Prajapati <keshavpj1711@gmail.com>
2024-07-16 00:44:49 +0530

    tic tac toe completed the final logic for anti-diagonal was the most challenging one

--- main.py
+++ main.py
@@ -20,11 +20,13 @@ def show_position():
 
 # To show the board 
 def board_layout():
+  print()
   print(f" {board[0][0]} | {board[0][1]} | {board[0][2]}")
   print("-----------")
   print(f" {board[1][0]} | {board[1][1]} | {board[1][2]}")
   print("-----------")
   print(f" {board[2][0]} | {board[2][1]} | {board[2][2]}")
+  print()
 
 
 # Checking if the place is available or occupied
@@ -33,6 +35,45 @@ def is_place_available(position):
     return False
   
   return True
+
+
+def check_win(m, position):
+  x = position[0] # getting row info
+  y = position[1] # getting column info
+
+  # Checking row
+  for i in range(0, 3):
+    if board[x][i] != m:
+      break
+    if i == 2:
+      print(f"{m} wins")
+      sys.exit()
+
+  # Checking col
+  for i in range(0, 3):
+    if board[i][y] != m:
+      break
+    if i == 2:
+      print(f"{m} wins")
+      sys.exit()
+
+  # Checking diagonal
+  if x == y:
+    for i in range(0, 3):
+      if board[i][i] != m:
+        break
+      if i == 2:
+        print(f"{m} wins")
+        sys.exit()
+
+  # Checking anti diagonal
+  if x+y == 2:
+    for i in range(0, 3):
+      if board[i][2-i] != m:
+        break
+      if i == 2:
+        print(f"{m} wins")
+        sys.exit()
 
 
 # This is to place the marker in the required position according to the player
@@ -46,13 +87,16 @@ def place_marker(player):
 
   user_input = int(input(f"Player {player}({marker}) enter position: "))
   pos = get_location(user_input)
+
   # until user enters a correct place
   while is_place_available(pos) == False:
     print("Place already occupied")
     user_input = int(input(f"Player {player}({marker}) enter position: "))
-    pos = get_location(user_input)  
+    pos = get_location(user_input)
 
   board[pos[0]][pos[1]] = marker
+  board_layout()
+  check_win(marker, pos)
 
 
 def get_location(position):
@@ -66,11 +110,19 @@ def get_location(position):
   return [row, col]
 
 
+def game_logic():
+  for i in range(0, 9):
+    if i%2 == 0:
+      place_marker(1)
+    else:
+      place_marker(2)
+
+
 def main():
-  print("Welcome!!")
+  print("\n----Tic-Tac-Toe----\n")
   show_position()
 
-  # Our next goal is to place elements
+  game_logic()
   
 
 if __name__ == "__main__":
